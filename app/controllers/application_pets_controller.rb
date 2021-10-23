e78d910fd1229801b010282d18007a0971502f5c
Philip Medlin <phmedlin@gmail.com>
2021-10-23 12:37:40 -0600

    Feat: Submit an app

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -7,7 +7,15 @@ class ApplicationPetsController < ApplicationController
   def create
     @application = Application.find_by_id("#{(params[:application_id])}")
     @pet = Pet.find_by_id("#{(params[:pet_id])}")
-    ApplicationPet.create!({application_id: "#{@application.id}", pet_id: "#{@pet.id}"})
-    redirect_to "/applications/#{@application.id}"
+    already_there = false
+    @application.application_pets.each do |a|
+      if "#{a.pet_id}" == params[:pet_id]
+        already_there = true
+      end
+    end
+    if already_there == false
+      ApplicationPet.create!({application_id: "#{@application.id}", pet_id: "#{@pet.id}"})
+      redirect_to "/applications/#{@application.id}"
+    end
   end
 end
