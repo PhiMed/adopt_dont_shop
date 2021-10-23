ed5db602104d98c5ebdcefab9dcacb62771f1f27
Philip Medlin <phmedlin@gmail.com>
2021-10-23 13:19:36 -0600

    Feat: add fuzzy search

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,7 @@
 class PetsController < ApplicationController
   def index
     if params[:search].present?
-      @pets = Pet.search(params[:search])
+      @pets = Pet.where("name like ?", "%#{params[:search]}%")
       if request.env["HTTP_REFERER"].include?("applications")
         redirect_to controller: 'applications', action: 'show',
          id: params[:redirect_to_application], search: "#{params[:search]}"
